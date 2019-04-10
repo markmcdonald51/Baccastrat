547441e95ad8c89e8a49380de6eac09be6f1b2e6
Mark McDonald <mark.mcdonald@gmail.com>
2019-04-10 13:29:54 +0700

    getting sgv images to work

--- app/helpers/simulations_helper.rb
+++ app/helpers/simulations_helper.rb
@@ -1,2 +1,22 @@
 module SimulationsHelper
+  CARD_SUITS =      ["Spades", "Clubs", "Hearts", "Diamonds"]
+  CARD_SUITS_SHORT =["S",      "C",     "H",      "D"]
+  
+  CARD_NAMES       = ["Ace", *(2..10), "Jack", "Queen", "King"].map(&:to_s)  
+  CARD_NAMES_SHORT = ["A",   *(2..10), "J",    "Q",     "K"].map(&:to_s)
+  
+    
+  def get_card_sgv(card)
+
+    
+    n, s = card.split('-')
+    #10_of_clubs.svg
+    
+    suit = CARD_SUITS[CARD_SUITS_SHORT.index(s)].downcase
+    name = CARD_NAMES[CARD_NAMES_SHORT.index(n)].downcase
+       
+    img_name = "#{name}_of_#{suit}.sgv"    
+    
+    return image_tag("/sgv-cards/#{img_name}", height: '32', width: '32')
+  end   
 end
